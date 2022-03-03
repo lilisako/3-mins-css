d77fefadded92e11ecf412439a26d8a4f930f6a3
risako <lilisako062397@gmail.com>
2022-03-03 20:38:59 +0900

    feat: add youtube

---
+++ pages/youtube.vue
@@ -0,0 +1,229 @@
+<template>
+  <div class="flex m-6">
+    <div class="w-1/12">
+      <div class="flex justify-center items-center">
+        <svg class="w-8 h-8"><g><path d="M21,6H3V5h18V6z M21,11H3v1h18V11z M21,17H3v1h18V17z" /></g></svg>
+      </div>
+      <div class="flex justify-center items-center my-5">
+        <div class="flex justify-center items-center w-full">
+          <div>
+            <div class="flex justify-center items-center">
+              <svg viewBox="0 0 24 24" class="w-6 h-6"><g><path d="M12,4.33l7,6.12V20H15V14H9v6H5V10.45l7-6.12M12,3,4,10V21h6V15h4v6h6V10L12,3Z" /></g></svg>
+            </div>
+            <div class="w-full text-center">
+              <p class="text-xs">
+                Home
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-center items-center my-5">
+        <div class="flex justify-center items-center w-full">
+          <div>
+            <div class="flex justify-center items-center">
+              <svg class="w-6 h-6"><g><path d="M9.8,9.8l-3.83,8.23l8.23-3.83l3.83-8.23L9.8,9.8z M13.08,12.77c-0.21,0.29-0.51,0.48-0.86,0.54 c-0.07,0.01-0.15,0.02-0.22,0.02c-0.28,0-0.54-0.08-0.77-0.25c-0.29-0.21-0.48-0.51-0.54-0.86c-0.06-0.35,0.02-0.71,0.23-0.99 c0.21-0.29,0.51-0.48,0.86-0.54c0.35-0.06,0.7,0.02,0.99,0.23c0.29,0.21,0.48,0.51,0.54,0.86C13.37,12.13,13.29,12.48,13.08,12.77z M12,3c4.96,0,9,4.04,9,9s-4.04,9-9,9s-9-4.04-9-9S7.04,3,12,3 M12,2C6.48,2,2,6.48,2,12s4.48,10,10,10s10-4.48,10-10S17.52,2,12,2 L12,2z" /></g></svg>
+            </div>
+            <div class="w-full text-center">
+              <p class="text-xs">
+                Explore
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center my-5">
+        <div class="flex justify-center items-center w-full">
+          <div>
+            <div class="flex justify-center items-center">
+              <svg class="w-8 h-8"><g><path d="M10 14.65v-5.3L15 12l-5 2.65zm7.77-4.33c-.77-.32-1.2-.5-1.2-.5L18 9.06c1.84-.96 2.53-3.23 1.56-5.06s-3.24-2.53-5.07-1.56L6 6.94c-1.29.68-2.07 2.04-2 3.49.07 1.42.93 2.67 2.22 3.25.03.01 1.2.5 1.2.5L6 14.93c-1.83.97-2.53 3.24-1.56 5.07.97 1.83 3.24 2.53 5.07 1.56l8.5-4.5c1.29-.68 2.06-2.04 1.99-3.49-.07-1.42-.94-2.68-2.23-3.25zm-.23 5.86l-8.5 4.5c-1.34.71-3.01.2-3.72-1.14-.71-1.34-.2-3.01 1.14-3.72l2.04-1.08v-1.21l-.69-.28-1.11-.46c-.99-.41-1.65-1.35-1.7-2.41-.05-1.06.52-2.06 1.46-2.56l8.5-4.5c1.34-.71 3.01-.2 3.72 1.14.71 1.34.2 3.01-1.14 3.72L15.5 9.26v1.21l1.8.74c.99.41 1.65 1.35 1.7 2.41.05 1.06-.52 2.06-1.46 2.56z" /></g></svg>
+            </div>
+            <div class="w-full text-center">
+              <p class="text-xs">
+                Shorts
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center my-5">
+        <div class="flex justify-center items-center w-full">
+          <div>
+            <div class="flex justify-center items-center">
+              <svg class="w-8 h-8"><g><path d="M10,18v-6l5,3L10,18z M17,3H7v1h10V3z M20,6H4v1h16V6z M22,9H2v12h20V9z M3,10h18v10H3V10z" /></g></svg>
+            </div>
+            <div class="w-full text-center">
+              <p class="text-xs">
+                Subscription
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center my-5">
+        <div class="flex justify-center items-center w-full">
+          <div>
+            <div class="flex justify-center items-center">
+              <svg class="w-8 h-8"><g><path d="M10,9.35,15,12l-5,2.65ZM12,6a54.36,54.36,0,0,0-7.56.38A1.53,1.53,0,0,0,3.38,7.44,24.63,24.63,0,0,0,3,12a24.63,24.63,0,0,0,.38,4.56,1.53,1.53,0,0,0,1.06,1.06A54.36,54.36,0,0,0,12,18a54.36,54.36,0,0,0,7.56-.38,1.53,1.53,0,0,0,1.06-1.06A24.63,24.63,0,0,0,21,12a24.63,24.63,0,0,0-.38-4.56,1.53,1.53,0,0,0-1.06-1.06A54.36,54.36,0,0,0,12,6h0m0-1s6.25,0,7.81.42a2.51,2.51,0,0,1,1.77,1.77A25.87,25.87,0,0,1,22,12a25.87,25.87,0,0,1-.42,4.81,2.51,2.51,0,0,1-1.77,1.77C18.25,19,12,19,12,19s-6.25,0-7.81-.42a2.51,2.51,0,0,1-1.77-1.77A25.87,25.87,0,0,1,2,12a25.87,25.87,0,0,1,.42-4.81A2.51,2.51,0,0,1,4.19,5.42C5.75,5,12,5,12,5Z" /></g></svg>
+            </div>
+            <div class="w-full text-center">
+              <p class="text-xs">
+                Originals
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center my-5">
+        <div class="flex justify-center items-center w-full">
+          <div>
+            <div class="flex justify-center items-center">
+              <svg class="w-8 h-8"><g><path d="M10 9.35L15 12l-5 2.65zM12 7a5 5 0 105 5 5 5 0 00-5-5m0-1a6 6 0 11-6 6 6 6 0 016-6zm0-3a9 9 0 109 9 9 9 0 00-9-9m0-1A10 10 0 112 12 10 10 0 0112 2z" /></g></svg>
+            </div>
+            <div class="w-full text-center">
+              <p class="text-xs">
+                YouTube Music
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-center items-center my-5">
+        <div class="flex justify-center items-center w-full">
+          <div>
+            <div class="flex justify-center items-center">
+              <svg class="w-8 h-8"><g><path d="M11,7l6,3.5L11,14V7L11,7z M18,20H4V6H3v15h15V20z M21,18H6V3h15V18z M7,17h13V4H7V17z" /></g></svg>
+            </div>
+            <div class="w-full text-center">
+              <p class="text-xs">
+                Library
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center my-5">
+        <div class="flex justify-center items-center w-full">
+          <div>
+            <div class="flex justify-center items-center">
+              <svg class="w-8 h-8"><g><path d="M17 18V19H6V18H17ZM16.5 11.4L15.8 10.7L12 14.4V4H11V14.4L7.2 10.6L6.5 11.3L11.5 16.3L16.5 11.4Z" /></g></svg>
+            </div>
+            <div class="w-full text-center">
+              <p class="text-xs">
+                Downloads
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-11/12">
+      <div class="flex justify-between items-center mb-2">
+        <div class="flex items-center mr-2">
+          <svg viewBox="0 0 97 20" preserveAspectRatio="xMidYMid meet" focusable="false" class="h-6"><g viewBox="0 0 97 20" preserveAspectRatio="xMidYMid meet"><g><path d="M27.9704 3.12324C27.6411 1.89323 26.6745 0.926623 25.4445 0.597366C23.2173 2.24288e-07 14.2827 0 14.2827 0C14.2827 0 5.34807 2.24288e-07 3.12088 0.597366C1.89323 0.926623 0.924271 1.89323 0.595014 3.12324C-2.8036e-07 5.35042 0 10 0 10C0 10 -1.57002e-06 14.6496 0.597364 16.8768C0.926621 18.1068 1.89323 19.0734 3.12324 19.4026C5.35042 20 14.285 20 14.285 20C14.285 20 23.2197 20 25.4468 19.4026C26.6769 19.0734 27.6435 18.1068 27.9727 16.8768C28.5701 14.6496 28.5701 10 28.5701 10C28.5701 10 28.5677 5.35042 27.9704 3.12324Z" fill="#FF0000" /><path d="M11.4275 14.2854L18.8475 10.0004L11.4275 5.71533V14.2854Z" fill="white" /></g><g id="youtube-red-paths"><path d="M40.0566 6.34524V7.03668C40.0566 10.4915 38.5255 12.5118 35.1742 12.5118H34.6638V18.5583H31.9263V1.42285H35.414C38.6078 1.42285 40.0566 2.7728 40.0566 6.34524ZM37.1779 6.59218C37.1779 4.09924 36.7287 3.50658 35.1765 3.50658H34.6662V10.4727H35.1365C36.6064 10.4727 37.1803 9.40968 37.1803 7.10253L37.1779 6.59218Z" /><path d="M46.5336 5.8345L46.3901 9.08238C45.2259 8.83779 44.264 9.02123 43.836 9.77382V18.5579H41.1196V6.0391H43.2857L43.5303 8.75312H43.6337C43.9183 6.77288 44.8379 5.771 46.0232 5.771C46.1949 5.7757 46.3666 5.79687 46.5336 5.8345Z" /><path d="M49.6567 13.2456V13.8782C49.6567 16.0842 49.779 16.8415 50.7198 16.8415C51.6182 16.8415 51.8228 16.1501 51.8439 14.7178L54.2734 14.8613C54.4568 17.5565 53.0481 18.763 50.6586 18.763C47.7588 18.763 46.9004 16.8627 46.9004 13.4126V11.223C46.9004 7.58707 47.8599 5.80908 50.7409 5.80908C53.6407 5.80908 54.3769 7.32131 54.3769 11.0984V13.2456H49.6567ZM49.6567 10.6703V11.5687H51.7193V10.675C51.7193 8.37258 51.5547 7.71172 50.6821 7.71172C49.8096 7.71172 49.6567 8.38669 49.6567 10.675V10.6703Z" /><path d="M68.4103 9.09902V18.5557H65.5928V9.30834C65.5928 8.28764 65.327 7.77729 64.7132 7.77729C64.2216 7.77729 63.7724 8.06186 63.4667 8.59338C63.4832 8.76271 63.4902 8.93439 63.4879 9.10373V18.5605H60.668V9.30834C60.668 8.28764 60.4022 7.77729 59.7884 7.77729C59.2969 7.77729 58.8665 8.06186 58.5631 8.57456V18.5628H55.7456V6.03929H57.9728L58.2221 7.63383H58.2621C58.8947 6.42969 59.9178 5.77588 61.1219 5.77588C62.3072 5.77588 62.9799 6.36854 63.288 7.43157C63.9418 6.34973 64.9225 5.77588 66.0443 5.77588C67.7564 5.77588 68.4103 7.00119 68.4103 9.09902Z" /><path d="M69.8191 2.8338C69.8191 1.4862 70.3106 1.09814 71.3501 1.09814C72.4132 1.09814 72.8812 1.54734 72.8812 2.8338C72.8812 4.22373 72.4108 4.57181 71.3501 4.57181C70.3106 4.56945 69.8191 4.22138 69.8191 2.8338ZM69.9837 6.03935H72.6789V18.5629H69.9837V6.03935Z" /><path d="M81.891 6.03955V18.5631H79.6849L79.4403 17.032H79.3792C78.7466 18.2573 77.827 18.7677 76.684 18.7677C75.0095 18.7677 74.2522 17.7046 74.2522 15.3975V6.0419H77.0697V15.2352C77.0697 16.3382 77.3002 16.7874 77.867 16.7874C78.3844 16.7663 78.8477 16.4582 79.0688 15.9902V6.0419H81.891V6.03955Z" /><path d="M96.1901 9.09893V18.5557H93.3726V9.30825C93.3726 8.28755 93.1068 7.7772 92.493 7.7772C92.0015 7.7772 91.5523 8.06177 91.2465 8.59329C91.263 8.76027 91.2701 8.9296 91.2677 9.09893V18.5557H88.4502V9.30825C88.4502 8.28755 88.1845 7.7772 87.5706 7.7772C87.0791 7.7772 86.6487 8.06177 86.3453 8.57447V18.5627H83.5278V6.0392H85.7527L85.9973 7.63139H86.0372C86.6699 6.42725 87.6929 5.77344 88.8971 5.77344C90.0824 5.77344 90.755 6.3661 91.0631 7.42913C91.7169 6.34729 92.6976 5.77344 93.8194 5.77344C95.541 5.77579 96.1901 7.0011 96.1901 9.09893Z" /><path d="M40.0566 6.34524V7.03668C40.0566 10.4915 38.5255 12.5118 35.1742 12.5118H34.6638V18.5583H31.9263V1.42285H35.414C38.6078 1.42285 40.0566 2.7728 40.0566 6.34524ZM37.1779 6.59218C37.1779 4.09924 36.7287 3.50658 35.1765 3.50658H34.6662V10.4727H35.1365C36.6064 10.4727 37.1803 9.40968 37.1803 7.10253L37.1779 6.59218Z" /></g></g></svg>
+        </div>
+        <div class="flex py-1 px-2 w-full h-10 border items-cneter">
+          programming
+        </div>
+        <div class="flex items-center">
+          <svg class="mx-2 w-8 h-8"><g><path d="M12 3C10.34 3 9 4.37 9 6.07V11.93C9 13.63 10.34 15 12 15C13.66 15 15 13.63 15 11.93V6.07C15 4.37 13.66 3 12 3ZM18.5 12H17.5C17.5 15.03 15.03 17.5 12 17.5C8.97 17.5 6.5 15.03 6.5 12H5.5C5.5 15.24 7.89 17.93 11 18.41V21H13V18.41C16.11 17.93 18.5 15.24 18.5 12Z" /></g></svg>
+          <svg class="mx-2 w-8 h-8"><g><path d="M14,13h-3v3H9v-3H6v-2h3V8h2v3h3V13z M17,6H3v12h14v-6.39l4,1.83V8.56l-4,1.83V6 M18,5v3.83L22,7v8l-4-1.83V19H2V5H18L18,5 z" /></g></svg>
+          <svg class="mx-2 w-8 h-8"><g><path d="M16,4v4h4V4H16z M19,7h-2V5h2V7z M16,10v4h4v-4H16z M19,13h-2v-2h2V13z M10,4v4h4V4H10z M13,7h-2V5h2V7z M10,10v4h4v-4H10z M13,13h-2v-2h2V13z M16,16v4h4v-4H16z M19,19h-2v-2h2V19z M10,16v4h4v-4H10z M13,19h-2v-2h2V19z M4,4v4h4V4H4z M7,7H5V5h2V7z M4,10 v4h4v-4H4z M7,13H5v-2h2V13z M4,16v4h4v-4H4z M7,19H5v-2h2V19z" /></g></svg>
+          <svg class="mx-2 w-8 h-8"><g><path d="M10,20h4c0,1.1-0.9,2-2,2S10,21.1,10,20z M20,17.35V19H4v-1.65l2-1.88v-5.15c0-2.92,1.56-5.22,4-5.98V3.96 c0-1.42,1.49-2.5,2.99-1.76C13.64,2.52,14,3.23,14,3.96l0,0.39c2.44,0.75,4,3.06,4,5.98v5.15L20,17.35z M19,17.77l-2-1.88v-5.47 c0-2.47-1.19-4.36-3.13-5.1c-1.26-0.53-2.64-0.5-3.84,0.03C8.15,6.11,7,7.99,7,10.42v5.47l-2,1.88V18h14V17.77z" /></g></svg>
+          <img class="mx-2 w-8 h-8 rounded-full" src="https://yt3.ggpht.com/bvsG8DABH27qOOV515j9MeJLpeLaQN08PAkTTeoz5uzKvgwjBeTm5PkbjVnkNYgUMQsyTXdPH-s=s88-c-k-c0x00ffffff-no-rj-mo">
+        </div>
+      </div>
+      <div class="p-4 bg-gray-50">
+        <div class="flex my-3">
+          <img class="w-1/3" src="https://i.ytimg.com/vi/zOjov-2OZ0E/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB645vj4oCWTlrqqFLrztWCaa_zKw">
+          <div class="mx-3">
+            <p class="text-lg font-bold">
+              Introduction to Programming and Computer Science - Full Course
+            </p>
+            <p class="text-sm text-gray-500">
+              3.2M views・1 year ago
+            </p>
+            <div class="flex items-center my-2">
+              <img class="mr-2 w-6 h-6 rounded-full" src="https://yt3.ggpht.com/ytc/AKedOLRyPFojwY3CXV5ks5TwPrt2VifQn-nYNfkgLvVPkw=s68-c-k-c0x00ffffff-no-rj">
+              <p class="text-sm text-gray-500">
+                freeCodeCamp.org
+              </p>
+            </div>
+            <p class="text-sm text-gray-500">
+              In this course, you will learn basics of computer programming and computer science. The concepts you learn apply to any and all ...
+            </p>
+          </div>
+        </div>
+        <div class="flex my-3">
+          <img class="w-1/3" src="https://i.ytimg.com/vi/CIRGjwYgdT4/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDi3g1uyMX7Ww3Em2K2x8CZwmX7JA">
+          <div class="mx-3">
+            <p class="text-lg font-bold">
+              Programming vs Coding - What's the difference?
+            </p>
+            <p class="text-sm text-gray-500">
+              960K views・1 year ago
+            </p>
+            <div class="flex items-center my-2">
+              <img class="mr-2 w-6 h-6 rounded-full" src="https://yt3.ggpht.com/2Ic5G-MGcf0BGlxLo86TkUZILkxKCenzjCszCyVJSRcmn5xkEgtLgs4CBCkaK2YNoNdX4A66=s68-c-k-c0x00ffffff-no-rj">
+              <p class="text-sm text-gray-500">
+                Aaron Jack
+              </p>
+            </div>
+            <p class="text-sm text-gray-500">
+              Programming vs coding -- my take on the difference. ‍ Join Freemote, the Freelance Developer Bootcamp
+            </p>
+          </div>
+        </div>
+        <div class="flex my-3">
+          <img class="w-1/3" src="https://i.ytimg.com/vi/FrFY6Y1MJBQ/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC7TGHCWwZ-jm3Fio_pReeyc5CkRg">
+          <div class="mx-3">
+            <p class="text-lg font-bold">
+              Self Taught Programmers... Listen Up.
+            </p>
+            <p class="text-sm text-gray-500">
+              982K views・10 months ago
+            </p>
+            <div class="flex items-center my-2">
+              <img class="mr-2 w-6 h-6 rounded-full" src="https://yt3.ggpht.com/ytc/AKedOLR2mPVgBIubx0-LLBrUYUAz8cIXrIEOqStUiCxBig=s68-c-k-c0x00ffffff-no-rj">
+              <p class="text-sm text-gray-500">
+                Kenny Gunderman
+              </p>
+            </div>
+            <p class="text-sm text-gray-500">
+              As a Self-Taught Programmer, I often get asked for my advice when it comes to pursuing this route. In this video, I give you my top ...
+            </p>
+          </div>
+        </div>
+        <div class="flex my-3">
+          <img class="w-1/3" src="https://i.ytimg.com/vi/vKsaO5IetS0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDYIJiT1zlATPrVhMgEz2MkpstKCw">
+          <div class="mx-3">
+            <p class="text-lg font-bold">
+              How to learn programming faster!
+            </p>
+            <p class="text-sm text-gray-500">
+              111K views・11 days ago
+            </p>
+            <div class="flex items-center my-2">
+              <img class="mr-2 w-6 h-6 rounded-full" src="https://yt3.ggpht.com/QMaMXCtmRUXUuIWb6sUMLi_ulWNCxPuVnu3I-3IkT1YCJs6XzcxKoOFrzx_WjVW65S46S_pTRQ=s68-c-k-c0x00ffffff-no-rj">
+              <p class="text-sm text-gray-500">
+                Tina Huang
+              </p>
+            </div>
+            <p class="text-sm text-gray-500">
+              In this video, I give you my best tips on how to learn programming faster! Affiliates ======================== 365 Data ...
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
