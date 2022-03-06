3aa0c303407d09e47e57bac8989c91dc380b63ad
risako <lilisako062397@gmail.com>
2022-03-06 20:30:05 +0900

    feat: add github

---
+++ pages/github.vue
@@ -0,0 +1,312 @@
+<template>
+  <div>
+    <!-- header -->
+    <div class="flex justify-between items-center py-2 w-full bg-black">
+      <div class="flex items-center">
+        <svg
+          height="32"
+          aria-hidden="true"
+          viewBox="0 0 16 16"
+          width="32"
+          data-view-component="true"
+          class="mx-4 text-white fill-current"
+        >
+          <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
+        </svg>
+        <p class="mx-2 text-white">
+          Product
+        </p>
+        <p class="mx-2 text-white">
+          Team
+        </p>
+        <p class="mx-2 text-white">
+          Enterprise
+        </p>
+        <p class="mx-2 text-white">
+          Explore
+        </p>
+        <p class="mx-2 text-white">
+          Marketplace
+        </p>
+        <p class="mx-2 text-white">
+          Pricing
+        </p>
+      </div>
+      <div class="flex items-center">
+        <p class="mx-2 text-white">
+          Sign in
+        </p>
+        <p class="p-2 mx-2 text-white rounded-lg border border-white">
+          Sign up
+        </p>
+      </div>
+    </div>
+    <!-- body -->
+    <div class="flex py-4">
+      <div class="px-4 w-1/3">
+        <img src="https://avatars.githubusercontent.com/u/33516104?v=4" class="rounded-full">
+        <p class="text-3xl font-bold">
+          R
+        </p>
+        <p class="text-xl text-gray-400">
+          lilisako
+        </p>
+        <button class="py-2 my-2 w-full bg-gray-100 rounded-xl shadow-sm">
+          Follow
+        </button>
+        <p class="text-gray-400">
+          <span class="font-bold text-black">17</span> followers・<span class="font-bold text-black">14</span> following
+        </p>
+        <hr class="my-4">
+        <p class="text-lg font-bold">
+          Highlights
+        </p>
+      </div>
+      <div class="w-2/3">
+        <div class="flex items-center mt-4">
+          <div class="flex items-center mx-2">
+            <svg
+              aria-hidden="true"
+              height="16"
+              viewBox="0 0 16 16"
+              width="16"
+              class="mx-2"
+            >
+              <path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z" />
+            </svg>
+            <p class="font-bold">
+              Overview
+            </p>
+          </div>
+          <div class="flex items-center mx-2">
+            <svg
+              aria-hidden="true"
+              height="16"
+              viewBox="0 0 16 16"
+              width="16"
+              class="mx-2"
+            >
+              <path fill-rule="evenodd" d="M0 1.75A.75.75 0 01.75 1h4.253c1.227 0 2.317.59 3 1.501A3.744 3.744 0 0111.006 1h4.245a.75.75 0 01.75.75v10.5a.75.75 0 01-.75.75h-4.507a2.25 2.25 0 00-1.591.659l-.622.621a.75.75 0 01-1.06 0l-.622-.621A2.25 2.25 0 005.258 13H.75a.75.75 0 01-.75-.75V1.75zm8.755 3a2.25 2.25 0 012.25-2.25H14.5v9h-3.757c-.71 0-1.4.201-1.992.572l.004-7.322zm-1.504 7.324l.004-5.073-.002-2.253A2.25 2.25 0 005.003 2.5H1.5v9h3.757a3.75 3.75 0 011.994.574z" />
+            </svg>
+            <p class="font-bold">
+              Repositories
+            </p>
+          </div>
+          <div class="flex items-center mx-2">
+            <svg
+              aria-hidden="true"
+              height="16"
+              viewBox="0 0 16 16"
+              width="16"
+              class="mx-2"
+            >
+              <path fill-rule="evenodd" d="M0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v3.585a.746.746 0 010 .83v8.085A1.75 1.75 0 0114.25 16H6.309a.748.748 0 01-1.118 0H1.75A1.75 1.75 0 010 14.25V6.165a.746.746 0 010-.83V1.75zM1.5 6.5v7.75c0 .138.112.25.25.25H5v-8H1.5zM5 5H1.5V1.75a.25.25 0 01.25-.25H5V5zm1.5 1.5v8h7.75a.25.25 0 00.25-.25V6.5h-8zm8-1.5h-8V1.5h7.75a.25.25 0 01.25.25V5z" />
+            </svg>
+            <p class="font-bold">
+              Projects
+            </p>
+          </div>
+          <div class="flex items-center mx-2">
+            <svg
+              aria-hidden="true"
+              height="16"
+              viewBox="0 0 16 16"
+              width="16"
+              class="mx-2"
+            >
+              <path fill-rule="evenodd" d="M0 1.75A.75.75 0 01.75 1h4.253c1.227 0 2.317.59 3 1.501A3.744 3.744 0 0111.006 1h4.245a.75.75 0 01.75.75v10.5a.75.75 0 01-.75.75h-4.507a2.25 2.25 0 00-1.591.659l-.622.621a.75.75 0 01-1.06 0l-.622-.621A2.25 2.25 0 005.258 13H.75a.75.75 0 01-.75-.75V1.75zm8.755 3a2.25 2.25 0 012.25-2.25H14.5v9h-3.757c-.71 0-1.4.201-1.992.572l.004-7.322zm-1.504 7.324l.004-5.073-.002-2.253A2.25 2.25 0 005.003 2.5H1.5v9h3.757a3.75 3.75 0 011.994.574z" />
+            </svg>
+            <p class="font-bold">
+              Packages
+            </p>
+          </div>
+          <div class="flex items-center mx-2">
+            <svg
+              aria-hidden="true"
+              height="16"
+              viewBox="0 0 16 16"
+              width="16"
+              class="mx-2"
+            >
+              <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z" />
+            </svg>
+            <p class="font-bold">
+              Stars
+            </p>
+          </div>
+        </div>
+        <div class="p-4 my-8 mx-2 rounded-xl border border-gray-300">
+          <p class="text-xs">
+            lilisako/README.md
+          </p>
+          <p class="my-2 text-2xl font-bold">
+            About me👋
+          </p>
+          <p>
+            🏫A 2nd-year master's student in computer science <br>
+            🖥Interested in web development and computer vision.
+          </p>
+        </div>
+        <div class="mx-2 text-xl">
+          <p>Pinned</p>
+          <div class="grid grid-cols-2 gap-4">
+            <div class="p-4 rounded-xl border border-gray-300">
+              <div class="flex items-center">
+                <svg
+                  aria-hidden="true"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  width="16"
+                  class="mr-2"
+                >
+                  <path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z" />
+                </svg>
+                <p class="text-sm font-bold text-blue-600">
+                  youtube-source-code
+                </p>
+              </div>
+              <p class="my-3 text-xs text-gray-500">
+                Source code for my youtube videos
+              </p>
+              <div class="flex items-center">
+                <div class="w-4 h-4 bg-green-500 rounded-full" />
+                <p class="mx-2 text-sm text-gray-500">
+                  Vue
+                </p>
+              </div>
+            </div>
+            <div class="p-4 rounded-xl border border-gray-300">
+              <div class="flex items-center">
+                <svg
+                  aria-hidden="true"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  width="16"
+                  class="mr-2"
+                >
+                  <path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z" />
+                </svg>
+                <p class="text-sm font-bold text-blue-600">
+                  3-mins-css
+                </p>
+              </div>
+              <p class="my-3 text-xs text-gray-500">
+                Source code for my youtube series "3 mins css"
+              </p>
+              <div class="flex items-center">
+                <div class="w-4 h-4 bg-green-500 rounded-full" />
+                <p class="mx-2 text-sm text-gray-500">
+                  Vue
+                </p>
+              </div>
+            </div>
+            <div class="p-4 rounded-xl border border-gray-300">
+              <div class="flex items-center">
+                <svg
+                  aria-hidden="true"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  width="16"
+                  class="mr-2"
+                >
+                  <path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z" />
+                </svg>
+                <p class="text-sm font-bold text-blue-600">
+                  teleuni
+                </p>
+              </div>
+              <p class="my-3 text-xs text-gray-500">
+                Free Online Courses In One Place - TELEUNI allows you to search a wide range of free online courses available on the Internet.
+              </p>
+              <div class="flex items-center">
+                <div class="w-4 h-4 bg-green-500 rounded-full" />
+                <p class="mx-2 text-sm text-gray-500">
+                  Vue
+                </p>
+              </div>
+            </div>
+            <div class="p-4 rounded-xl border border-gray-300">
+              <div class="flex items-center">
+                <svg
+                  aria-hidden="true"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  width="16"
+                  class="mr-2"
+                >
+                  <path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z" />
+                </svg>
+                <p class="text-sm font-bold text-blue-600">
+                  fakerjp
+                </p>
+              </div>
+              <p class="my-3 text-xs text-gray-500">
+                Generates fake data for Japanese names, numbers, addresses, and more. Note: Please be aware that this gem is under heavy development.
+              </p>
+              <div class="flex items-center">
+                <div class="w-4 h-4 bg-red-800 rounded-full" />
+                <p class="mx-2 text-sm text-gray-500">
+                  Ruby
+                </p>
+              </div>
+            </div>
+            <div class="p-4 rounded-xl border border-gray-300">
+              <div class="flex items-center">
+                <svg
+                  aria-hidden="true"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  width="16"
+                  class="mr-2"
+                >
+                  <path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z" />
+                </svg>
+                <p class="text-sm font-bold text-blue-600">
+                  design-pattern-ruby
+                </p>
+              </div>
+              <p class="my-3 text-xs text-gray-500">
+                「Java言語で学ぶデザインパターン入門 」のruby実装
+              </p>
+              <div class="flex items-center">
+                <div class="w-4 h-4 bg-red-800 rounded-full" />
+                <p class="mx-2 text-sm text-gray-500">
+                  Ruby
+                </p>
+              </div>
+            </div>
+            <div class="p-4 rounded-xl border border-gray-300">
+              <div class="flex items-center">
+                <svg
+                  aria-hidden="true"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  width="16"
+                  class="mr-2"
+                >
+                  <path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z" />
+                </svg>
+                <p class="text-sm font-bold text-blue-600">
+                  newsforest
+                </p>
+              </div>
+              <p class="my-3 text-xs text-gray-500">
+                Political discussion platform in Rails
+              </p>
+              <div class="flex items-center">
+                <div class="w-4 h-4 bg-red-800 rounded-full" />
+                <p class="mx-2 text-sm text-gray-500">
+                  Ruby
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
