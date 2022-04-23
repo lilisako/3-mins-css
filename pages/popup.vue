206b0e57f869a88b91c96c5b8a2f8409213e36ef
risako <lilisako062397@gmail.com>
2022-04-23 17:36:28 +0900

    fix: add popup

---
+++ pages/popup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex items-center w-screen h-screen font-garamond bg-gray-200">
+    <div class="relative p-8 mx-auto w-80 h-64 bg-white rounded-xl drop-shadow-md">
+      <div class="absolute -top-16 left-[100px] w-32 h-32 bg-white rounded-full border-8 border-[#B7D0FF]" />
+      <div class="absolute -top-24 left-16">
+        <img src="~/static/popup/mail.png" width="200">
+      </div>
+      <div class="flex items-end h-full">
+        <div>
+          <p class="text-xl text-center">
+            To continue, approve the notifiction sent to:
+          </p>
+          <p class="my-3 text-5xl font-bold text-center">
+            ***-***-**12
+          </p>
+          <p class="text-center underline">
+            Resend notification
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/popup2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex items-center w-screen h-screen font-garamond bg-gray-200">
+    <div class="relative py-6 px-8 mx-auto w-80 h-64 bg-white rounded-xl drop-shadow-md">
+      <div class="absolute -top-16 left-[100px] w-32 h-32 bg-white rounded-full border-8 border-[#8588F8]" />
+      <div class="absolute -top-20 left-20">
+        <img src="~/static/popup/docs.png" width="180">
+      </div>
+      <div class="flex items-end h-full">
+        <div>
+          <p class="pb-8 text-3xl text-center underline">
+            Your email has been confirmed!
+          </p>
+          <button class="py-2 w-full text-white bg-black rounded-md">
+            Go to Homepage
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
