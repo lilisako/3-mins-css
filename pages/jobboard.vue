f108143d68464453c57cf01fd5e95737c4a13505
risako <lilisako062397@gmail.com>
2022-04-24 19:48:53 +0900

    feat: add jobboard

---
+++ pages/jobboard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex items-center w-screen h-screen bg-gray-200 ">
+    <div class="flex p-6 mx-auto bg-white rounded-xl drop-shadow">
+      <div>
+        <img src="~/static/jobboard/google.png" width="80" class="rounded-xl border-[4px] border-black">
+        <img src="~/static/jobboard/Bookmark.png" width="30" class="my-4">
+      </div>
+      <div class="ml-4">
+        <p class="text-xl font-bold">
+          Software Engineer
+        </p>
+        <div class="my-4 font-bold leading-5 text-gray-400">
+          <p>Google</p>
+          <p>Tokyo, Japan(On-site)</p>
+        </div>
+        <div class="text-sm">
+          <div class="flex items-center my-1">
+            <img src="~/static/jobboard/people.png" width="15" class="mr-2">
+            <p>Full-time / Entry level</p>
+          </div>
+          <div class="flex items-center my-1">
+            <img src="~/static/jobboard/Building.png" width="15" class="mr-2">
+            <p>10,001+ employees</p>
+          </div>
+          <div class="flex items-center my-1">
+            <img src="~/static/jobboard/Network.png" width="15" class="mr-2">
+            <p>38 company alumni</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
