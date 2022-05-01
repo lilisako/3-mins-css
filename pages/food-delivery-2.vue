5cb4eafd7340d80c2ff62b60bd3ce611237be7ac
risako <lilisako062397@gmail.com>
2022-05-01 15:43:56 +0900

    feat: add food delivery

--- pages/food-delivery-2.vue
+++ pages/food-delivery-2.vue
@@ -1,7 +1,61 @@
 <template>
   <div class="flex justify-center items-center w-screen h-screen bg-gray-200">
-    <div class="w-[300px] h-[649px] bg-white rounded-lg">
-      <div />
+    <div class="overflow-hidden px-4 w-[300px] h-[649px] font-bold bg-[#FFE81C] rounded-lg">
+      <div class="relative">
+        <div class="flex justify-between items-center my-4">
+          <img src="~/static/delivery/back.png" width="25">
+          <img src="~/static/delivery/basket.png" width="35">
+        </div>
+        <img src="~/static/delivery/yamada-ramen.png">
+        <div class="p-2 my-2 bg-white rounded-xl border-2 border-black">
+          <p class="text-lg">
+            Yamada Ramen
+          </p>
+          <div class="flex justify-between items-end">
+            <div>
+              <p class="text-xs">
+                25-35 mins
+              </p>
+              <p class="text-xs">
+                Delivery fee ¥250
+              </p>
+            </div>
+            <div class="py-1 w-6 h-6 text-xs text-center text-white bg-black rounded-full">
+              4.7
+            </div>
+          </div>
+        </div>
+        <div>
+          <p>Menu</p>
+          <div class="flex p-2 my-2 bg-white rounded-xl border-2 border-black">
+            <img src="~/static/delivery/ramen1.png">
+            <div class="relative mx-2 w-full">
+              <p>Tonkotsu Ramen</p>
+              <p class="absolute right-0 bottom-0">
+                ¥1000
+              </p>
+            </div>
+          </div>
+          <div class="flex p-2 my-2 bg-white rounded-xl border-2 border-black">
+            <img src="~/static/delivery/ramen2.png">
+            <div class="relative mx-2 w-full">
+              <p>Shrimp Ramen</p>
+              <p class="absolute right-0 bottom-0">
+                ¥1000
+              </p>
+            </div>
+          </div>
+          <div class="flex p-2 my-2 bg-white rounded-xl border-2 border-black">
+            <img src="~/static/delivery/ramen3.png">
+            <div class="relative mx-2 w-full">
+              <p>Special Chicken Ramen</p>
+              <p class="absolute right-0 bottom-0">
+                ¥1250
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- pages/food-delivery-3.vue
+++ pages/food-delivery-3.vue
@@ -1,7 +1,73 @@
 <template>
   <div class="flex justify-center items-center w-screen h-screen bg-gray-200">
-    <div class="w-[300px] h-[649px] bg-white rounded-lg">
-      <div />
+    <div class="overflow-hidden w-[300px] h-[649px] font-bold bg-[#FFE81C] rounded-lg">
+      <div class="mx-4">
+        <div class="flex justify-between items-center my-4">
+          <img src="~/static/delivery/back.png" width="25">
+        </div>
+        <div class="p-4 bg-white rounded-xl border-2 border-black">
+          <div class="flex justify-between items-center">
+            <p>CREDIT CARD</p>
+            <img src="~/static/delivery/visa.png" width="80">
+          </div>
+          <div class="flex justify-between mt-12">
+            <p>4111-****-****-1111</p>
+            <p>10/22</p>
+          </div>
+        </div>
+        <div class="p-4 my-4 bg-white rounded-xl border-2 border-black">
+          <div class="flex my-2">
+            <img src="~/static/delivery/ramen1.png" width="80">
+            <div class="relative ml-3 w-full">
+              <p class="text-sm">
+                Tonkotsu Ramen
+              </p>
+              <p class="absolute right-0 bottom-0">
+                ¥1000
+              </p>
+            </div>
+          </div>
+          <div class="flex my-2">
+            <img src="~/static/delivery/ramen2.png" width="80">
+            <div class="relative ml-3 w-full">
+              <p class="text-sm">
+                Shrimp Ramen
+              </p>
+              <p class="absolute right-0 bottom-0">
+                ¥1000
+              </p>
+            </div>
+          </div>
+          <div class="flex my-2">
+            <img src="~/static/delivery/ramen3.png" width="80">
+            <div class="relative ml-3 w-full">
+              <p class="text-sm">
+                Special Chicken Ramen
+              </p>
+              <p class="absolute right-0 bottom-0">
+                ¥1250
+              </p>
+            </div>
+          </div>
+          <div class="flex my-2 border-b-2 border-black">
+            <div class="relative w-full">
+              <p class="text-sm">
+                Delivery Fee
+              </p>
+              <p class="absolute right-0 bottom-0">
+                ¥250
+              </p>
+            </div>
+          </div>
+          <div class="flex justify-between text-xl">
+            <p>Total</p>
+            <p>¥3500</p>
+          </div>
+        </div>
+        <button class="py-3 w-full text-xl text-white bg-black rounded-lg">
+          Complete Order
+        </button>
+      </div>
     </div>
   </div>
 </template>
