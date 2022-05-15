8e445fad3b3f9e25c4b47c09c751d61b5e8193b8
risako <lilisako062397@gmail.com>
2022-05-15 13:48:02 +0900

    lint: user-profile

--- pages/user-profile.vue
+++ pages/user-profile.vue
@@ -1,55 +1,89 @@
 <template>
   <div class="flex justify-center items-center w-screen h-screen bg-gray-200">
-    <div class="overflow-scroll w-[300px] h-[649px] font-vt323 bg-[#298061] rounded-lg px-4 pt-8">
+    <div class="overflow-scroll px-4 pt-8 w-[300px] h-[649px] font-vt323 bg-[#298061] rounded-lg">
       <div class="w-full h-[87px] bg-[#41DDFF] rounded-t-lg border-4 border-black">
-        <p class="text-3xl mx-2">#OpenToWork</p>
+        <p class="mx-2 text-3xl">
+          #OpenToWork
+        </p>
       </div>
-      <div class="w-full bg-[#EA8C00] border-l-4 border-r-4 border-b-4 border-black pt-[70px] pb-3 relative">
-        <img src="~/static/user-profie/user.png"  class="absolute -top-8 left-[80px]" width="100"/>
-        <p class="text-center text-3xl">Jess</p>
-        <p class="text-center text leading-4">A Software Engineer in Japan! My hobby is designing & coding cool web stuff! </p>
+      <div class="relative pt-[70px] pb-3 w-full bg-[#EA8C00] border-r-4 border-b-4 border-l-4 border-black">
+        <img src="~/static/user-profie/user.png" class="absolute -top-8 left-[80px]" width="100">
+        <p class="text-3xl text-center">
+          Jess
+        </p>
+        <p class="leading-4 text-center text">
+          A Software Engineer in Japan! My hobby is designing & coding cool web stuff!
+        </p>
       </div>
-      <div class="bg-[#FFEA7E] border-l-4 border-r-4 border-b-4 border-black py-4 relative w-full">
-        <p class="text-center text-3xl">Experience</p>
+      <div class="relative py-4 w-full bg-[#FFEA7E] border-r-4 border-b-4 border-l-4 border-black">
+        <p class="text-3xl text-center">
+          Experience
+        </p>
         <div class="flex my-2">
-          <div class="w-12 h-12 bg-[#FF3A3A] border-4 border-black mx-2 flex items-center justify-center">
-            <p class="text-2xl">💻</p>
+          <div class="flex justify-center items-center mx-2 w-12 h-12 bg-[#FF3A3A] border-4 border-black">
+            <p class="text-2xl">
+              💻
+            </p>
           </div>
           <div class="">
-            <p class="text-xl">Software Engineer</p>
-            <p class="text-sm">Company A・2022/04 - Current</p>
+            <p class="text-xl">
+              Software Engineer
+            </p>
+            <p class="text-sm">
+              Company A・2022/04 - Current
+            </p>
           </div>
         </div>
         <div class="flex my-2">
-          <div class="w-12 h-12 bg-[#FFBC3A] border-4 border-black mx-2 flex items-center justify-center">
-            <p class="text-2xl">💻</p>
+          <div class="flex justify-center items-center mx-2 w-12 h-12 bg-[#FFBC3A] border-4 border-black">
+            <p class="text-2xl">
+              💻
+            </p>
           </div>
           <div class="">
-            <p class="text-xl">Software Engineer Intern</p>
-            <p class="text-sm">Company B・2021/04 - 2022/03</p>
+            <p class="text-xl">
+              Software Engineer Intern
+            </p>
+            <p class="text-sm">
+              Company B・2021/04 - 2022/03
+            </p>
           </div>
         </div>
         <div class="flex my-2">
-          <div class="w-12 h-12 bg-[#A036F3] border-4 border-black mx-2 flex items-center justify-center">
-            <p class="text-2xl">🏫</p>
+          <div class="flex justify-center items-center mx-2 w-12 h-12 bg-[#A036F3] border-4 border-black">
+            <p class="text-2xl">
+              🏫
+            </p>
           </div>
           <div class="">
-            <p class="text-xl">Computer Science <br/>Master’s degree</p>
-            <p class="text-sm">Company C・2020/04 - 2022/03</p>
+            <p class="text-xl">
+              Computer Science <br>Master’s degree
+            </p>
+            <p class="text-sm">
+              Company C・2020/04 - 2022/03
+            </p>
           </div>
         </div>
         <div class="flex my-2">
-          <div class="w-12 h-12 bg-[#6456FF] border-4 border-black mx-2 flex items-center justify-center">
-            <p class="text-2xl">🏫</p>
+          <div class="flex justify-center items-center mx-2 w-12 h-12 bg-[#6456FF] border-4 border-black">
+            <p class="text-2xl">
+              🏫
+            </p>
           </div>
           <div class="">
-            <p class="text-xl">Computer Science <br/>Master’s degree</p>
-            <p class="text-sm">Company D・2016/04 - 2020/03</p>
+            <p class="text-xl">
+              Computer Science <br>Master’s degree
+            </p>
+            <p class="text-sm">
+              Company D・2016/04 - 2020/03
+            </p>
           </div>
         </div>
       </div>
-      <div class="bg-[#9F95E0] border-l-4 border-r-4 border-b-4 border-black py-4 relative w-full">
-        <p class="text-center text-3xl">Skills</p>
+      <div class="relative py-4 w-full bg-[#9F95E0] border-r-4 border-b-4 border-l-4 border-black">
+        <p class="text-3xl text-center">
+          Skills
+        </p>
       </div>
     </div>
   </div>
